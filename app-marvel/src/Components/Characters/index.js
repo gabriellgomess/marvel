fc824f072a3e46b9b3cfb46b9670a8597f018c7c
gabriellgomess <51470523+gabriellgomess@users.noreply.github.com>
2022-03-29 15:39:09 -0300

    implantação mail

--- app-marvel/src/Components/Characters/index.js
+++ app-marvel/src/Components/Characters/index.js
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from "react";
 import api from "../../Services/api";
 import "./index.css";
 import Modal from "react-modal";
-import "./modal.css"
+import "./modal.css";
+import ContactUs from "../Mail/SendMail";
 
 
 Modal.setAppElement('#root')
@@ -20,7 +21,7 @@ const Characters = () => {
                 .get('/comics')
                 .then((response) => {                    
                     setCharacters(response.data.data.results)                    
-            }) .catch((err) => {
+            }).catch((err) => {
                 console.log(err)
         });
             }catch(e){
@@ -34,14 +35,20 @@ const Characters = () => {
     // state responsável pela ativação da modal
     const[modalIsOpen, setIsOpen] = useState(false)
     // state que receberá o conteúdo da modal
-    const[detalhes, setDetalhes] = useState([])    
-
+    const[detalhes, setDetalhes] = useState({
+        "titulo": '',
+        "descrição": '',
+        "personagens": '',
+        "imagem": '',
+        "ext": ''  
+    })    
+    const[favoritos, setFavoritos] = useState([])
     // função de abertura da modal com os dados recebidos e atribuindo ao state detalhes
     function handleOpenModal(text){    
-        setDetalhes({
+        setDetalhes({            
             "titulo": text.title,
             "descrição": text.variantDescription,
-            // "personagens": person,
+            "personagens": text.characters,
             "imagem": text.thumbnail.path,
             "ext": text.thumbnail.extension            
         })
@@ -65,7 +72,28 @@ const Characters = () => {
         }
     }
     // ------------------------------------------------------------
-    // função responsável pela pesquisa
+    // favoritos --------------------------------------------------
+    const handleFavorite = (fav) => {
+        if(!favoritos.includes(fav)){
+            setFavoritos([...favoritos, fav])
+            
+        }else{
+            console.log("repetido")
+        }        
+    }
+    // const handleDelFavorite = (favoritos,delFav) => {
+    const handleDelFavorite = (id) => {
+            var result = favoritos.filter(function(el) {
+              return el.id === id;
+            });              
+            for(var elemento of result){
+              var index = favoritos.indexOf(elemento);    
+              favoritos.splice(index, 1) ;
+            }            
+          }
+    // }
+    // ------------------------------------------------------------    
+    //função responsável pela pesquisa
     const handleSearch = (e) => {
         if(!e.target.value){
             setResponse(characters)        
@@ -74,11 +102,10 @@ const Characters = () => {
             const resSearch = characters.filter((valor) => {
             return valor.title.includes(e.target.value)            
             })            
-            setResponse(resSearch)
-            console.log(resSearch)
+            setResponse(resSearch)            
         }
     }
-    // -----------------------------------------------------------
+    // -----------------------------------------------------------  
     // validação  
     if(response.length > 0){
         var resp = response
@@ -88,42 +115,62 @@ const Characters = () => {
 
     return(
         <>
-        {console.log(characters)}
-        
-        <div className="searchContainer">         
-               <input id="pesquisar" className="form-control" placeholder="Pesquisar" onChange={handleSearch} type="text" />
-                                             
-        </div>    
-               
-        <div className="container">
-            <ul>                   
-            {resp.map(character => {
-                return(                    
-                    <li className="card" key={character.id}>                                                                    
-                        <div className="text-card">                            
-                            <h3>{character.title}</h3>
-                            <p>{character.characters.items.name}</p>
+            <div className="searchContainer">         
+                <input id="pesquisar" className="form-control" placeholder="Pesquisar" onChange={handleSearch} type="text" />                                             
+            </div>   
+            <div className="container">
+                <ul>                          
+                {resp.map(character => {
+                    return(                    
+                        <li className="card" key={character.id}>                                                                    
+                            <div className="text-card">                            
+                                <h3>{character.title}</h3>
+                                <p>{character.characters.items.name}</p>
+                            </div>
+                            
+                            <div className="btn-group">
+                                <button className="btn btn-dark btn-sm more-info" onClick={() => handleOpenModal(character)}>Saiba Mais</button>
+                                <button className="btn btn-success btn-sm" onClick={() => handleFavorite(character)}>Selecionar</button>
+                                <button className="btn btn-danger btn-sm" onClick={() => handleDelFavorite(character.id)}>Remover</button>
+                            </div>                                                    
+                            
+                        </li>
+                    ) 
+                                
+                })}           
+                <ContactUs dados={favoritos} />
+                </ul> 
+                <Modal isOpen={modalIsOpen} onRequestClose={handleCloseModal} style={customStyles}>
+                    <div className="headerModal">
+                        <i onClick={handleCloseModal} className="fa-solid fa-xmark"></i>
+                    </div>
+                    <div className="title-modal d-flex justify-content-center mb-3">
+                        <h4>{detalhes.titulo}</h4>
+                    </div>
+                    <div className="container">                
+                        <div className="row d-flex justify-content-around w-100">
+                            <div className="col-6">
+                                <div className="imgModal">
+                                    <img width="200px" src={`${detalhes.imagem}.${detalhes.ext}`} alt="imagem comics"></img> 
+                                </div>
+                            </div>
+                            <div className="col-6">
+                                <div className="mainModal">                    
+                                    <p>{detalhes.variantDescription}</p>
+                                    <h4>Personagens</h4>
+                                    <ul>{                                   
+                                        detalhes.personagens.items?.map(personagem => {
+                                            return(
+                                                <li key={personagem.id}>{personagem.name}</li>
+                                            )
+                                        })
+                                    }</ul>
+                                </div>
+                            </div>
                         </div>
-                        <button className="btn btn-dark btn-sm more-info" onClick={() => handleOpenModal(character)}>Saiba Mais</button>                                             
-                    </li>
-                ) 
-                               
-            })}
-            </ul> 
-            <Modal isOpen={modalIsOpen} onRequestClose={handleCloseModal} style={customStyles}>
-                <div className="headerModal">
-                    <i onClick={handleCloseModal} className="fa-solid fa-xmark"></i>
-                </div>
-                <div className="imgModal">
-                    <img width="200px" src={`${detalhes.imagem}.${detalhes.ext}`}></img> 
-                </div>
-                <div className="mainModal">
-                    <h4>{detalhes.titulo}</h4>
-                    <p>{detalhes.variantDescription}</p>
-                    {/* <p>{detalhes.personagem}</p> */}  
-                </div>        
-            </Modal>
-        </div>
+                    </div>        
+                </Modal>
+            </div>
         </>
     );
 }
---
+++ app-marvel/src/Components/Mail/SendMail.js
@@ -0,0 +1,47 @@
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+
+const ContactUs = (props) => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_o2wtk2a', 'template_non13ug', form.current, 'TMNLCu_E7C7UNYD1p')
+      .then((result) => {
+          console.log(result.text);
+          e.target.reset();            
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+  
+  return (
+    <div className='d-flex justify-content-around'>        
+        <div className='col-sm-12 col-md-12 col-lg-6'>
+            <h4 className='text-light'>Compartilhar por email <i className="fa-solid fa-envelope"></i></h4>
+            <form ref={form} onSubmit={sendEmail}>
+                <input placeholder='E-mail' className='form-control  mt-1' type="email" name="email" />
+                {props.dados.map((cont) => { 
+                    return(
+                        <div key={cont.id} className='form-group'>            
+                            <input placeholder='Nome' className='form-control mt-1' type="hidden" name="my_title" value={"<h1>"+cont.title+"</h1><p>Descrição: "+cont.description+"</p><img width='100px' src="+cont.thumbnail.path+"."+cont.thumbnail.extension+" /<br /><br />"} />       
+                        </div>
+                    )  
+                    })
+                }
+                <input className='btn btn-primary mt-2' type="submit" value="Enviar" />
+            </form>
+        </div>
+        <div className='col-sm-12 col-md-12 col-lg-6 d-flex align-items-center flex-column'>
+            <h4 className='text-light '>Selecionados <i className="fa-solid fa-check"></i></h4>
+            <ul>
+            {props.dados.map((sel) => {return <li key={sel.id} className='text-light'>{sel.title}</li>})}
+            </ul>
+        </div>        
+    </div>      
+  );
+};
+
+export default ContactUs;
